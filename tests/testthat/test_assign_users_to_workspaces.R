08bfdce8f3cf3f3a73d9f1e63417691d246568ba
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2021-05-18 22:30:25 -0400

    Add overlooked workspaces unit tests

---
+++ tests/testthat/test_assign_users_to_workspaces.R
@@ -0,0 +1,67 @@
+# INPUTS
+
+# users is a character vector
+test_that("users is a character vector", {
+
+    expect_error(
+        assign_users_to_workspaces(
+            users = 1,
+            workspaces = "primary"
+        )
+    )
+
+})
+
+# each element of users is a GUID
+test_that("each element of users is a GUID", {
+
+    expect_error(
+        assign_users_to_workspaces(
+            users = c("7bdf95abab1b4d46b818cdf7546e049f", "abc"),
+            workspaces = "primary"
+        )
+    )
+
+})
+
+# workspaces is a character vector
+test_that("workspaces is a character vector", {
+
+    expect_error(
+        assign_users_to_workspaces(
+            users = "7bdf95abab1b4d46b818cdf7546e049f",
+            workspaces = 1
+        )
+    )
+
+})
+
+# action %in% c("Assign", "Add", "Remove")
+test_that('action %in% c("Assign", "Add", "Remove")', {
+
+    expect_error(
+        assign_users_to_workspaces(
+            users = "7bdf95abab1b4d46b818cdf7546e049f",
+            workspaces = "primary",
+            action = "Nothing"
+        )
+    )
+
+})
+
+# OUTPUTS
+
+# issues message on success/failure of operation
+vcr::use_cassette("assign_users_to_workspaces_msg", {
+    test_that("issues message on success/failure of operation", {
+
+        expect_message(
+            assign_users_to_workspaces(
+                users = "7bdf95abab1b4d46b818cdf7546e049f",
+                workspaces = "primary",
+                action = "Assign"
+            )
+        )
+
+    })
+})

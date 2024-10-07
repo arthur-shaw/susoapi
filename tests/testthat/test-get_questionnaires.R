2dd9132c236832f3ff96369ca9393c5e8c0d6223
Arthur Shaw <jashaw2002@gmail.com>
2024-10-07 16:20:05 -0400

    Add tests that URL can be provided with and without terminal slash

--- tests/testthat/test-get_questionnaires.R
+++ tests/testthat/test-get_questionnaires.R
@@ -8,6 +8,36 @@ test_that("issues error if invalid workspace name", {
 
 })
 
+testthat::test_that("Accepts URL w/o terminal slash", {
+
+    vcr::use_cassette("get_when_url_does_not_end_in_slash", {
+        x <- get_questionnaires(
+            server = Sys.getenv("SUSO_SERVER")
+        )
+    })
+
+    testthat::expect_s3_class(
+        x,
+        class = c("tbl_df","tbl","data.frame")
+    )
+
+})
+
+testthat::test_that("Accepts URL w/ terminal slash", {
+
+    vcr::use_cassette("get_when_url_ends_in_slash", {
+        x <- get_questionnaires(
+            server = paste0(Sys.getenv("SUSO_SERVER"), "/")
+        )
+    })
+
+    testthat::expect_s3_class(
+        x,
+        class = c("tbl_df","tbl","data.frame")
+    )
+
+})
+
 # outputs
 
 # returns df with expected columns
